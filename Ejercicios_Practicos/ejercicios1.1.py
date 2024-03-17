05676c16f59f5dc14697afd30ec7b3cdfa0d02fc
Smkg911 <alejoespana123@gmail.com>
2024-03-16 21:29:44 -0500

    avances pro

--- Ejercicios_Practicos/ejercicios1.1.py
+++ Ejercicios_Practicos/ejercicios1.1.py
@@ -8,7 +8,8 @@ crudoDalto = 3.5
 # difirencia con duracion 
 diferenciaMin = 100 - cursoDalto / minimoH *100
 diferenciaPromedio = 100 - cursoDalto / promedioH * 100
-diferenciaMax = 100 - cursoDalto *1000 // maximoH / 10 
+# diferenciaMax = 100 - cursoDalto *1000 // maximoH / 10 
+diferenciaMax = round(100 - cursoDalto / maximoH *100, 2)
 
 print (f"el curso de dalto es un {diferenciaMin}% mas rapido que el mas rapido")
 print (f"el curso de dalto es un {diferenciaMax}% mas rapido que el mas lento")
@@ -16,8 +17,10 @@ print (f"el curso de dalto es un {diferenciaPromedio}% mas rapido que el mas pro
 
 print("------------------------------------------------------") 
 # crudos 
-basuraDalto = 100 - cursoDalto * 1000 // crudoDalto / 10 
-basuraOtros = 100 - promedioH * 1000 // crudoPromedio / 10
+# basuraDalto = round(100 - cursoDalto * 1000 // crudoDalto / 10,3)
+# basuraOtros = round(100 - promedioH * 1000 // crudoPromedio / 10,3)
+basuraDalto = round(100 - cursoDalto / crudoDalto * 100, 2)
+basuraOtros = round(100 - promedioH / crudoPromedio * 100, 2)
 
 print("------------------------------------------------------") 
 print(f"esta es la basura de dalto {basuraDalto}")
@@ -26,5 +29,5 @@ print(f"esta es la basura de los cursos basura {basuraOtros}")
 # diferencia de los cursos si los cursos duraran 10 horas
 print("------------------------------------------------------") 
 
-print (f"mirar 10 horas de este curso equivalen a ver {promedioH *100 // cursoDalto / 10}horas de otro curso ")
-print (f"mirar 10 horas de otros cursos equivalen a ver {cursoDalto *100 // promedioH / 10}horas del curso de dalto ")
+print (f"mirar 10 horas de este curso equivalen a ver {round(promedioH *100 // cursoDalto / 10,10)} horas de otro curso ")
+print (f"mirar 10 horas de otros cursos equivalen a ver {round(cursoDalto *100 // promedioH / 10, 10)}horas del curso de dalto ")
---
+++ funciones/build_in.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3, 54, 6, 44, 99]
+
+# enconctrando el numero mayor de una lista y el numero mas pequenio
+print(max(numeros))
+print(min(numeros))
+
+#redondeando a 6 decimales
+print(round(50.546461316516323215646652311233213216646, 2))
+
+# retorna false en caso que le demos un 0, vacio, falso, none 
+resultado = bool(154)
+print (resultado)
