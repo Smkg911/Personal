cbcad435edbf47637b57684bd4f773267543284e
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-03-19 19:32:43 -0500

    Update build_in.py

--- funciones/build_in.py
+++ funciones/build_in.py
@@ -8,5 +8,13 @@ print(min(numeros))
 print(round(50.546461316516323215646652311233213216646, 2))
 
 # retorna false en caso que le demos un 0, vacio, falso, none 
-resultado = bool(154)
+resultado = bool(False)
 print (resultado)
+
+# retorna True si todos los valores son verdaderos
+resultadoAll = all([254, 'true', 546.41, None])
+print(resultadoAll)
+
+#suma todos los valores de un iterable 
+sumaTotal = sum(numeros)
+print(sumaTotal) 
