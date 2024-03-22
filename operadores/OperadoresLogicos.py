b64ac1ebd3360bc1ff9c9543b2f7880b881e6e3d
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-03-22 15:01:19 -0500

    Create OperadoresLogicos.py

---
+++ operadores/OperadoresLogicos.py
@@ -0,0 +1,18 @@
+'''------------------------------------------------------
+# and - or - not (bool)
+------------------------------------------------------'''
+#and (and)
+True and True # = True 
+False and True # = False
+True and False # = False
+False and False # = False 
+
+#or (or) 
+True or True # = True 
+True or False # = True 
+False or True # = True 
+False or False # = False 
+
+#not (not) 
+not True # = False 
+not False # = True 
