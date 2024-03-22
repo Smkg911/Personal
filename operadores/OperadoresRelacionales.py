0ab1f0fc7eb7b78f07759d0a94da3caf752b1f6b
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-03-22 14:31:47 -0500

    Create OperadoresRelacionales.py

---
+++ operadores/OperadoresRelacionales.py
@@ -0,0 +1,17 @@
+#igual que 
+valorUnitario == 55.75
+
+#diferente de 
+tipo != TipoDrogueria 
+
+#menor que 
+20 < 29
+
+#mayor que 
+20 > 15
+
+#menor o igual que 
+20 <= 20
+
+#mayor o igual que 
+25 >= 16
